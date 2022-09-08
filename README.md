f40943d0778714d7520cb4868d59f78feede9724
James Stabb <jjstabb@gmail.com>
2022-09-09 02:51:05 +1200

    initial working commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Overview
+A starter boilerplate setup for basic frontend projects using github.com/thecodercoder/frontend-boilerplate but modified to use Panini for html templating.
+# Main packages
+Gulp, Sass, Panini + BrowserSync
+# Run
+gulp bs 
---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+// Initialize modules
+ const { src, dest, watch, series, parallel } = require('gulp');
+
+// Importing the Gulp packages 
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const replace = require('gulp-replace');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+const panini = require('panini');  
+const browsersync = require('browser-sync').create();
+
+
+// define file paths
+const files = {
+	scssPath: 'src/scss/**/*.scss',
+	jsPath: 'src/scripts/**/*.js',
+	htmlPath: 'src/**/*.html',	 
+};
+
+ 
+// Sass task 
+function scssTask() {
+	return src(files.scssPath, { sourcemaps: true }) 
+		.pipe(sass()) 
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest('dist', { sourcemaps: '.' })); 
+}
+
+// JS task
+function jsTask() {
+	return src(
+		[
+			files.jsPath,
+		],
+		{ sourcemaps: true }
+	)
+	.pipe(concat('all.js'))
+	.pipe(terser())
+	.pipe(dest('dist', { sourcemaps: '.' }));
+}
+
+
+// HTML Task (Panini)
+function htmlTask() {
+	return  src('src/pages/**/*.{html,hbs,handlebars}')
+    .pipe(panini({
+      root: 'src/pages/',
+      layouts: 'src/layouts/',
+      partials: 'src/partials/',
+      helpers: 'src/helpers/',
+      data: 'src/data/'
+    }))
+	.pipe(dest('dist', { sourcemaps: '.' }));
+	
+}
+function resetPages(cb) {
+	panini.refresh();
+	cb();
+}
+ 
+
+// Cachebust
+function cacheBustTask() {
+	var cbString = new Date().getTime();
+	return src(['dist/index.html'])
+		.pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+		.pipe(dest('.'));
+}
+
+// Browsersync 
+function browserSyncServe(cb) {
+	browsersync.init({
+		server: {
+			baseDir: 'dist',
+		},
+		port: 3000 
+	});
+	cb(); 
+}
+function browserSyncReload(cb) {  
+	browsersync.reload();
+	cb();  
+}
+
+// Watch task 
+function watchTask() {  
+	watch(
+		[ files.scssPath, files.jsPath],
+		{ interval: 1000, usePolling: true }, //Makes docker work
+		series( parallel(scssTask, jsTask), browserSyncReload)
+	);
+	watch(
+		[ files.htmlPath],
+		{ interval: 1000, usePolling: true }, //Makes docker work
+		series(  resetPages, htmlTask, browserSyncReload)
+	);
+  
+}
+
+// Export the Gulp tasks (command >> gulp bs)
+exports.bs = series(
+	parallel(scssTask, jsTask),
+	cacheBustTask,
+	browserSyncServe,
+	htmlTask,
+	resetPages,	
+	watchTask
+);
